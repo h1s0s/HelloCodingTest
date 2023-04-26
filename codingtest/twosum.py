14ba93f0f38b6ce014cf6906bcda5779d80c4f66
h1s0s <hs94122@naver.com>
2023-04-26 17:59:46 +0900

    코테1 12:00까지

--- codingtest/twosum.py
+++ codingtest/twosum.py
@@ -1,4 +1,5 @@
-def twoSum(nums, target):
+def twoSum1(nums, target):
+    #반복문을 사용한 방법
     n = len(nums)  # nums.length
     for i in range(n-1):
         for j in range(i+1, n):
@@ -6,5 +7,14 @@ def twoSum(nums, target):
                 return True
     return False
 
+def twoSum2(nums, target):
+    #정렬&투포인터를 사용한 방법
+    n = len(nums)
+    nums.sort() #시간복잡도 O(n log n) 일단 정렬
+    for left in range(0, n):
+        for right in range (n-1):
+            return True
+    return False
+
 if __name__ == "__main__":
-    print(twoSum(nums=[4, 1, 9, 7, 5, 3, 16], target=14))
+    print(twoSum1(nums=[4, 1, 9, 7, 5, 3, 16], target=14))
