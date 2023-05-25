243c81b5cbafc79832a0eb8da0c5b791cb61951d
h1s0s <hs94122@naver.com>
2023-05-25 15:19:53 +0900

    dictionary 알고리즘

--- codingtest/dictionary/twosum.py
+++ codingtest/dictionary/twosum.py
@@ -1,9 +1,11 @@
 def twoSum(nums, target):
     memo = {}
     # 딕셔너리에 nums 값을 하나하나 저장
+    # 복잡도 O(n)
     for v in nums:
         memo[v] = True
 
+    # 복잡도 O(n)
     for v in nums:
         if (target - v) in d:
             return True
