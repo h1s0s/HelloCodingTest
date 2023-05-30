4d20e37d401b8792581de662b179e5bccd044283
h1s0s <hs94122@naver.com>
2023-05-30 14:22:56 +0900

    큐

---
+++ codingtest/queue/Queue.py
@@ -0,0 +1,14 @@
+#stack 2개로 queue 구현
+class Queue(object):
+    def __init__(self):
+        self.instack=[]
+        self.outstack=[]
+
+    def enqueue(self, element):
+        self.instack.append(element)
+
+    def dequeue(self):
+        if not self.outstack:
+            while self.instack:
+                self.outstack.append(self.instack.pop())
+        return self.outstack.pop()
