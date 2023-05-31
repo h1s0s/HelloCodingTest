189930fd33a2fd88fe37905cd2b4535d1a1305c6
h1s0s <hs94122@naver.com>
2023-05-31 14:58:44 +0900

    lv1 추억점수

--- programmers/memorytrip.py
+++ programmers/memorytrip.py
@@ -1,12 +1,12 @@
 def solution(name, yearning, photo):
-    answer = []
-    friend = {}
+    answer = [] #배열
+    friend = {} #딕셔너리
     for i in range(len(yearning)):
         friend[name[i]] = yearning[i]
     for i in photo:
         result = 0
         for j in i:
-            if j in friend:
+            if j in friend: #friend에 j key값이 있는지?
                 result += friend[j]
         answer.append(result)
     return answer
@@ -16,3 +16,6 @@ if __name__ == "__main__":
     yearning = [5, 10, 1, 3]
     photo = [["may", "kein", "kain", "radi"], ["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]
     print(solution(name, yearning, photo))
+
+def 다른풀이1(이름, 점수, 사진):
+    return [sum(점수[이름.index(j)] for j in i if j in 이름) for i in 사진]
