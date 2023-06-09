b587558ef4a0b31ddd6f5ea80bbd71e618e5cce3
h1s0s <hs94122@naver.com>
2023-06-09 15:40:38 +0900

    그리디

--- backjoon/그리디/곱하기혹은더하기.py
+++ backjoon/그리디/곱하기혹은더하기.py
@@ -1,14 +1,11 @@
 def solution(s):
-    answer = 0
     array = list(map(int, s))
+    array = [item for item in array if item != 0] #리스트 컴프리헨션으로 0 빼는 로직 추가함
+    answer = array[0]
     for i in range(1, len(array)):
-        if i == 1:
-            if array[i-1] == 0:
-                answer += array[i]
-            else:
-                answer = array[i-1]
-                answer *= array[i]
-        else :
+        if array[i] <= 1:
+            answer += array[i]
+        else:
             answer *= array[i]
     return answer
 
