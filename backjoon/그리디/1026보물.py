f2faa14e528b189180e67433a73cd491c8229383
h1s0s <hs94122@naver.com>
2023-06-12 15:55:50 +0900

    그리디

---
+++ backjoon/그리디/1026보물.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    N = input()
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    A.sort()
+    B.sort(reverse=True)
+    sum = 0
+    for i in range(len(A)):
+        sum += A[i] * B[i]
+    print(sum)
