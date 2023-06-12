b139481c489ecf1c95668bf428aacdf9e53ca50b
h1s0s <hs94122@naver.com>
2023-06-12 17:36:39 +0900

    그리디

---
+++ backjoon/그리디/13164행복유치원.py
@@ -0,0 +1,17 @@
+# if __name__ == "__main__":
+#     # N, K = map(int, input.split())
+#     # W = list(map(int, input.split()))
+#     N, K = map(int, "5 3".split())
+#     W = list(map(int, "1 3 5 6 10".split()))
+#     R = {}
+#     count = 0
+#     for i in range(len(W)-1):
+#         R[i] = (W[i+1]- W[i])
+#
+#     while True:
+#         W[min(R.keys())].remove()
+#         W[min(R.keys())].remove()
+#         if W != null:
+#             break
+#
+#    print(count)
