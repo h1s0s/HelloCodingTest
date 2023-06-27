e89a44da82cadce402d9b4c359688e4c10b6edca
h1s0s <hs94122@naver.com>
2023-06-27 15:54:14 +0900

    백준

---
+++ backjoon/구현/7568덩치.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    N = int(input())
+    for i in range(N)
+        A = list(map(int, input().split()))
+    for x in range()
--- programmers/lv2/ex.py
+++ programmers/lv2/ex.py
@@ -3,4 +3,16 @@ def solution():
     return answer
 
 if __name__ == "__main__":
-    print(solution())
+    i = 0
+    sum = 0
+    while i < 10:
+        i = i+1
+        if i % 3 == 0:
+            sum -= i
+        elif i % 3 == 1:
+            sum += i
+        else:
+            sum *= i
+
+    print(sum)
+
