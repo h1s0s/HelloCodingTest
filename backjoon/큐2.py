3db97566f57f66c4c2de233403a0e4003f7f55bf
한상선 <hs94122@naver.com>
2023-07-30 20:59:43 +0900

    18258 큐2

--- backjoon/큐2.py
+++ backjoon/큐2.py
@@ -1,23 +1,29 @@
 
 if __name__ == "__main__":
-    N = input() #입력횟수
-    A = list(map(int, input().split()))
+    N = int(input()) #입력횟수
     list = list()
-    for i in range(len(A)):
+    for _ in range(N):
+        i = input()
         if i == "pop":
             if len(list) == 0:
                 print("-1")
             else :
-                list.pop(0)
+                print(list.pop(0))
         elif i == "size":
-            print(list.size)
+            print(len(list))
         elif i == "empty":
             word = 1 if len(list) == 0 else 0
             print(word)
         elif i == "front":
-            print("a")
+            if len(list) == 0:
+                print("-1")
+            else:
+                print(list[0])
         elif i == "back":
-            print("a")
+            if len(list) == 0:
+                print("-1")
+            else:
+                print(list[len(list)-1])
         else:
             value = i.split()
-            list.append(value[2])
+            list.append(value[1])
